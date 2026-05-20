86bbd23e2e73e6f49044be571bb45d79a80218a6
Jase Scott <144859802+JScott3234@users.noreply.github.com>
2026-05-20 16:42:02 -0500

    Page Loading Fixes
    I now understand what it means to Over-Engineer something

--- pages/scripts/head.js
+++ pages/scripts/head.js
@@ -1,33 +1,4 @@
-// Inject Header - wayyy easier to maintain now lol
-var header = `<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark box-shadow pb-3 pt-2"
-            style="background-color: #162957;">
-            <div class="container">
-                <a class="navbar-brand" href="../index.html" style="color: mintcream;">Home</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                    <ul class="navbar-nav flex-grow-1">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../pages/projects.html">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../pages/education.html">Education</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../pages/leadership.html">Leadership</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../pages/research.html">Research</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>`;
-
-const head = document.querySelector('#header');
-head.insertAdjacentHTML('afterbegin', header);
+// Injecting the header creates significant performance issues
 
 // Inject Footer
 var footer = `<footer class="footer">
